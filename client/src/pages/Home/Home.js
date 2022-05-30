9a0e9e53561fa60a3db8fe44f103fff8dc141116
Crystal Tai <crystaltai315@gmail.com>
2022-05-29 23:25:08 -0700

    Installed styled-components and test styled button

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Home = () => {
-  return <div>I'm the HOME page!</div>;
+  return (
+    <div>
+      I'm the HOME page! and <Button>CLICK</Button>
+    </div>
+  );
 };
+
+const Button = styled.button``;
 
 export default Home;
