5059e951f42c1fb6acfe0a15f20fc0a1c64ddc06
Crystal Tai <crystaltai315@gmail.com>
2022-06-09 22:51:37 -0700

    Created constants file to contain all navigation links

---
+++ client/src/components/Header/constants.js
@@ -0,0 +1,39 @@
+import GithubLogo from '../../images/github.svg';
+import LinkedinLogo from '../../images/linkedin.svg';
+import TwitterLogo from '../../images/twitter.svg';
+
+const navLinks = [
+  {
+    url: '/about',
+    text: 'About',
+  },
+  {
+    url: '/stack',
+    text: 'Stack',
+  },
+  {
+    url: '/repos',
+    text: 'Repos',
+  },
+  {
+    url: '/changelog',
+    text: 'Changelog',
+  },
+  {
+    url: 'https://github.com/crystaltai',
+    text: 'Github logo',
+    icon: GithubLogo,
+  },
+  {
+    url: 'https://linkedin.com/in/crystaltai',
+    text: 'Linkedin logo',
+    icon: LinkedinLogo,
+  },
+  {
+    url: 'https://twitter.com/crystaltaixyz',
+    text: 'Twitter logo',
+    icon: TwitterLogo,
+  },
+];
+
+export default navLinks;
