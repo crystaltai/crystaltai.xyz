d7131f36ccb7835dabb623eaa24150bd7540efe9
Crystal Tai <crystaltai315@gmail.com>
2022-06-10 19:30:35 -0700

    Fixed inconsistent page width across different pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
-import About from './pages/About/About';
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Repos from './pages/Repos/Repos';
 import './App.css';
 
 const App = () => {
@@ -10,25 +11,23 @@ const App = () => {
     <Router>
       <div className='content'>
         <Header />
-        <div className='main-content'>
-          <Switch>
-            <Route exact path='/'>
-              <Home />
-            </Route>
-            <Route path='/about'>
-              <About />
-            </Route>
-            <Route path='/stack'>
-              <div>coming soon</div>
-            </Route>
-            <Route path='/repos'>
-              <div>coming soon</div>
-            </Route>
-            <Route path='/changelog'>
-              <div>coming soon</div>
-            </Route>
-          </Switch>
-        </div>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/stack'>
+            <div>coming soon</div>
+          </Route>
+          <Route path='/repos'>
+            <Repos />
+          </Route>
+          <Route path='/changelog'>
+            <div>coming soon</div>
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
